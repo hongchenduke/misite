70404131175d61033a967f9a744d9ed4fa053842
duke <523559493@qq.com>
2024-05-27 10:29:39 +0800

    修改task app的admin展示,调整展示字段,可编辑字段,每页展示数量,搜索字段等

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Task
+
+
 # Register your models here.
 
-admin.site.register(Task)
+# 定义task app在admin后台的展示
+class TaskAdmin(admin.ModelAdmin):
+    # 展示字段
+    list_display = ['name', 'status']
+    # 可编辑字段
+    list_editable = ('status',)
+    # 每页展示条数
+    list_per_page = 5
+    # 可搜索字段
+    search_fields = ['name']
+
+
+# 注册task app至后台admin
+admin.site.register(Task, TaskAdmin)
