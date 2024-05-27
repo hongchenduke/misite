17bda49c92b8eabc3707af3843f1c350679baefa
duke <523559493@qq.com>
2024-05-27 11:42:05 +0800

    采用函数视图的方法,对tasks app 增加了添加,更新,删除任务的功能

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,20 @@
-from django.urls import path, include
+from django.urls import path, include,re_path
 from . import views
 
 # namespace
 app_name = 'tasks'
 
 urlpatterns = [
+    # 查询任务列表
     path('', views.task_list, name='task_list'),
+    # 创建一个任务
+    path('create/',views.task_create,name='task_create'),
+    # 查询单个任务
+    re_path(r'^(?P<pk>\d+)/$', views.task_detail, name='task_detail'),
+    # 更新单个任务
+    re_path(r'^(?P<pk>\d+)/update/$', views.task_update, name='task_update'),
+    # 删除单个任务
+    re_path(r'^(?P<pk>\d+)/delete/$', views.task_delete, name='task_delete'),
+
 ]
+
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from .models import Task
+from .forms import TaskForm
 
 
 # Create your views here.
-
-# 任务清单
+# 查询任务列表
 def task_list(request):
     # 从数据库获取Task对象列表
     tasks = Task.objects.all()
     # 指定渲染模板并向模版传递数据
     return render(request, "tasks/task_list.html", {"tasks": tasks, })
+
+
+# 创建一个任务
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("tasks:task_list"))
+    else:
+        form = TaskForm()
+    return render(request, "tasks/task_form.html", {"form": form, })
+
+
+# 查询单个任务
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, "tasks/task_detail.html", {"task": task, })
+
+
+# 更新单个任务
+def task_update(request, pk):
+    task_obj = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(instance=task_obj, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("tasks:task_detail", args=[pk, ]))
+    else:
+        form = TaskForm(instance=task_obj)
+    return render(request, "tasks/task_form.html", {"form": form, "object": task_obj})
+
+
+# 删除单个任务
+
+def task_delete(request, pk):
+    task_obj = get_object_or_404(Task, pk=pk)
+    task_obj.delete()
+    return redirect(reverse("tasks:task_list"))
